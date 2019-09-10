dadb42e1e80f44ff73a1402f8e1b4e35a5a45aea
guolei0528 <942297338@qq.com>
2019-09-10 15:37:34 +0800

    233

--- src/com/gl/main/javasenior/anonclass/Main.java
+++ src/com/gl/main/javasenior/anonclass/Main.java
@@ -14,6 +14,8 @@ public class Main {
 		System.out.println("--------------------");
 		
 		System.out.println("--------------------");
+		
+		System.out.println("--------------------");
 		p.cont().value();
 		p.dest("南京").readLabel();
 	}
